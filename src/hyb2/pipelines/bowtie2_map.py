0a85fba10b5a383214bb53d062a147a01a4971c7
TomHarcus <tomharcus17@gmail.com>
2026-08-03 15:58:14 +0100

    started on the bowtie2 elements

---
+++ src/hyb2/pipelines/bowtie2_map.py
@@ -0,0 +1,7 @@
+""" Port of bowtie2_fasta2sam, bowtie2_fastq.gz2sam, and bowtie2_fastq2sam all condensed into one script
+
+"""
+
+def bowtie2_map():
+
+    return
---
+++ src/hyb2/stages/make_hyb_db_2.py
@@ -0,0 +1,19 @@
+""" Port of make_hyb_db_2
+
+"""
+
+from pathlib import Path
+import subprocess
+from hyb2.stages.fasta2tab import fasta_to_tab
+
+def make_hyb_db_2(in_fasta):
+
+    if not Path(in_fasta).is_file():
+        raise FileNotFoundError(f"error: {in_fasta} file not found")
+
+    prefix = in_fasta.replace(".fasta", "", 1)
+    subprocess.run(["bowtie2-build", in_fasta, prefix], check=True)
+
+    tab = in_fasta.replace("fasta", "tab", 1)
+    with open(in_fasta) as fin:
+        Path(tab).write_text(fasta_to_tab(fin.read()))
